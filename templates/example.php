849b7d581e9d792699b47637021b068ace662e90
comtom <tomas@comtom.com.ar>
2014-10-09 01:02:49 -0300

    fix dirs

--- templates/example.php
+++
@@ -1,7 +0,0 @@
-<br><br><br><br>
-<div class="ui segment" style="width:450px; margin:65px auto;">
-    <h2>Welcome to</h2>
-    <h1>Transparent<br>Framework</h1>
-
-    <p>Proudly crafted in Mar del Plata, Buenos Aires, Argentina</p>
-</div>
